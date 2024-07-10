3fdf99e40eeee85ede9497a7dfe45b15a8709ce1
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-10 11:40:53 +0300

    Add files via upload

---
+++ WorkingWithAbstraction/Exercise/CardsWithPower_03/Card.java
@@ -0,0 +1,13 @@
+package OOP.WorkingWithAbstraction.Exercise.CardsWithPower_03;
+
+public class Card {
+    private int power;
+
+    public Card(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
