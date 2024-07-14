8ac52e6cbaa2eb37d0c0266bad116d7d70ca2af0
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-14 21:26:18 +0300

    Add files via upload

---
+++ Encapsulation/Exercise/PizzaCalories_04/Main.java
@@ -0,0 +1,12 @@
+package OOP.Encapsulation.Exercise.PizzaCalories_04;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
