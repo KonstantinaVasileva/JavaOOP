9558bdf25df8f632472185f94c1ab319935bd1d1
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-15 16:57:30 +0300

    Add files via upload

---
+++ Inheritance/Lab/RandomArrayList_03/Main.java
@@ -0,0 +1,8 @@
+package OOP.Inheritance.Lab.RandomArrayList_03;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+
+    }
+}
