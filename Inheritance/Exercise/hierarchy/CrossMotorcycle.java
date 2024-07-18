dd84391f3af61423ad92e8f701b8a43a2e07b9bf
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-18 15:32:07 +0300

    Add files via upload

---
+++ Inheritance/Exercise/hierarchy/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package OOP.Inheritance.Exercise.hierarchy;
+
+public class CrossMotorcycle extends Motorcycle{
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
