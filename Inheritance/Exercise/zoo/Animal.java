9dbdb80015b6804adf02273f80bae8b3bdaae0b3
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-18 15:51:14 +0300

    Add files via upload

---
+++ Inheritance/Exercise/zoo/Animal.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance.Exercise.zoo;
+
+public class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Inheritance/Exercise/zoo/Gorilla.java
@@ -0,0 +1,5 @@
+package OOP.Inheritance.Exercise.zoo;
+
+public class Gorilla extends Mammal{
+
+}
