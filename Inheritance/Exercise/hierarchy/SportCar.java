a1f79eb5510c3a5e32b485eaf39c725755ad0d26
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-18 15:31:29 +0300

    Add files via upload

---
+++ Inheritance/Exercise/hierarchy/SportCar.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance.Exercise.hierarchy;
+
+public class SportCar extends Car{
+    final static double DEFAULT_FUEL_CONSUMPTION = 10;
+    public SportCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
