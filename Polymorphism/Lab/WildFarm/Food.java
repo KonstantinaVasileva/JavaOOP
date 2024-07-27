c73c7a2a8c018bb1afafeee929eb3edf5832e7eb
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-27 12:04:45 +0300

    Add files via upload

---
+++ Polymorphism/Lab/WildFarm/Food.java
@@ -0,0 +1,10 @@
+package OOP.Polymorphism.Lab.WildFarm;
+
+public abstract class Food {
+    protected Integer quantity;
+
+    public Food(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+}
