3a793937ee835759c8b31511396262b31eb3a4ae
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-07-27 12:05:05 +0300

    Add files via upload

---
+++ Polymorphism/Lab/WildFarm/Felime.java
@@ -0,0 +1,7 @@
+package OOP.Polymorphism.Lab.WildFarm;
+
+public abstract class Felime extends Mammal {
+    public Felime(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
