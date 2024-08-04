0b258171461cf6e0ab01fdeb05b1e732a645b8ce
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-08-04 17:14:24 +0300

    Add files via upload

---
+++ Exam preparation/16.04.20204/High Quality Structure/Main.java
@@ -0,0 +1,12 @@
+package OOP.ExamPreparation.Apr16_2024.dolphinarium;
+
+import OOP.ExamPreparation.Apr16_2024.dolphinarium.core.Engine;
+import OOP.ExamPreparation.Apr16_2024.dolphinarium.core.EngineImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new EngineImpl();
+        engine.run();
+
+    }
+}
---
+++ Exam preparation/16.04.20204/High Quality Structure/common/Command.java
@@ -0,0 +1,12 @@
+package OOP.ExamPreparation.Apr16_2024.dolphinarium.common;
+public enum Command {
+    AddPool,
+    BuyFood,
+    AddFoodToPool,
+    AddDolphin,
+    FeedDolphins,
+    PlayWithDolphins,
+    GetStatistics,
+    Exit
+}
+
